94a1359f87ba485d0ec09bbcac245775b6545438
vikramgrewal <vik.grewal1992@gmail.com>
2018-03-26 22:32:05 -0700

    Initial commit for brain game

---
+++ Whats your guess?/Controllers/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Whats your guess?
+//
+//  Created by Vikram Work/School on 3/25/18.
+//  Copyright © 2018 Vikram Work/School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view, typically from a nib.
+
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+
+      
+
+}
+
---
+++ Whats your guess?/Models/Game.swift
@@ -0,0 +1,11 @@
+//
+//  Game.swift
+//  Whats your guess?
+//
+//  Created by Vikram Work/School on 3/26/18.
+//  Copyright © 2018 Vikram Work/School. All rights reserved.
+//
+
+import Foundation
+
+class CFHost
